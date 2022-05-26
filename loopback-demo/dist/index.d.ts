a2e5dd83960ce457c6c651d7c086375efc237046
Deepak Singh Solanki <deepaksinghsolanki1989@gmail.com>
2022-05-26 23:33:28 +0530

    Delete loopback-demo directory

--- loopback-demo/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, LoopbackDemoApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<LoopbackDemoApplication>;
--- loopback-demo/src/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './base-entity.model';
-export * from './country.model';
-export * from './state.model';
-export * from './city.model';
